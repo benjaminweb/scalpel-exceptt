22868028968673bade852168754183d3f4254328
Benjamin Weber <one@bwe.im>
2023-12-07 11:13:04 +0100

    first working example with textComment

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,41 @@
 module Main where
 
+import Data.Text (Text)
+import Text.HTML.Scalpel
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print $ scrapeStringLike exampleHtml comments
+
+-- adopted from <https://github.com/fimad/scalpel/blob/master/examples/example-from-documentation/Main.hs>
+exampleHtml :: Text
+exampleHtml = "<html>\
+\    <body>\
+\        <div class='comments'>\
+\            <div class='comment container'>\
+\                <span class='comment author'>Sally</span>\
+\                <div class='comment text'>Woo hoo!</div>\
+\            </div>\
+\            <div class='comment container'>\
+\                <span class='comment author'>Bill</span>\
+\                <img class='comment image' src='http://example.com/cat.gif' />\
+\            </div>\
+\            <div class='comment container'>\
+\                <span class='comment author'>Susan</span>\
+\                <div class='comment text'>WTF!?!</div>\
+\            </div>\
+\        </div>\
+\    </body>\
+\</html>"
+
+type Author = Text
+
+data Comment = TextComment Author Text deriving (Show, Eq)
+
+textComment :: Scraper Text Comment
+textComment = do
+  author <- text $ "span" @: [hasClass "author"]
+  commentText <- text $ "div" @: [hasClass "text"]
+  return $ TextComment author commentText
+
+comments :: Scraper Text [Comment]
+comments = chroots ("div" @: [hasClass "container"]) textComment
